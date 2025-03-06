12e7099f039a2ce544378bb4b17054bae976a4ff
Thomas <boy1.thoomas@gmail.com>
2025-03-06 17:31:03 +0100

    feat(add): add data from questions and answers

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { Col, Container, Row } from "react-bootstrap";
 import { Header } from "./components/Header";
 
 export const App = () => {
   return (
     <>
       <Header />
+      <Container>
+        <Row className="my-4">
+          <Col>
+            <h1>Quiz Interactif</h1>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
